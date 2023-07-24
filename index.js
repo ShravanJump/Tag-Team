2a11b64074548a5fe17ba9de9c5b929d5616fc6b
ShravanJump <cst.21bctg94@silicon.ac.in>
2023-07-24 12:22:59 +0530

    bookings page added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function openwebsite(){
-    window.location.href="/index.html";
+    window.location.href="index.html";
 }
 
 function redirect(){
-    window.location.href="/index.html"
+    window.location.href="Bookings/bookings.html"
 }
