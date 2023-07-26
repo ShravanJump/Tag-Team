5a0e0452e27ce3de9b73d25ee66b425e7002b4c1
ipsitamoh07 <138458933+ipsitamoh07@users.noreply.github.com>
2023-07-26 22:39:25 +0530

    price updated

--- bill.js
+++ bill.js
@@ -1,10 +1,20 @@
-
-function calculateTotalAmount() {
-    const rooms = parseInt(document.getElementById('rooms').value);
-    const totalAmount = rooms * 100; // change it according to hotel
-    document.getElementById('totalAmount').value = totalAmount;
-  }
+const roomTypeDropdown = document.getElementById('room-type');
+    // const confirmationDiv = document.getElementById('confirmation');
   
+    const priceInput = document.getElementById('price');
+
+  const roomPrices = {
+    standard: 100,    //change it accordingly
+    premium: 200,
+    honeymoon: 300,
+  };
+
+  function calculatePrice() {
+    const selectedRoomType = roomTypeDropdown.value;
+    const selectedPrice = roomPrices[selectedRoomType];
+    priceInput.value = selectedPrice;
+  }
+  roomTypeDropdown.addEventListener('change', calculatePrice);
   function calculateNumberOfNights() {
     const checkInDate = new Date(document.getElementById('checkIn').value);
     const checkOutDate = new Date(document.getElementById('checkOut').value);
@@ -13,8 +23,11 @@ function calculateTotalAmount() {
     const numberOfNights = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
   
     document.getElementById('numberOfNights').value = numberOfNights;
+    
   }
+
   
+  calculateTotalAmount();
 
   function handleFormSubmit(event) {
     event.preventDefault();
