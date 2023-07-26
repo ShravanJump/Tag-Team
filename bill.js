3b38f6111cfb34ade78f913d57e254e223a2ee03
ipsitamoh07 <138458933+ipsitamoh07@users.noreply.github.com>
2023-07-26 22:55:25 +0530

    some modifications

--- bill.js
+++ bill.js
@@ -1,33 +1,39 @@
 const roomTypeDropdown = document.getElementById('room-type');
-    // const confirmationDiv = document.getElementById('confirmation');
-  
-    const priceInput = document.getElementById('price');
+const numRoomsInput = document.getElementById('rooms');
+const priceInput = document.getElementById('price');
 
   const roomPrices = {
-    standard: 100,    //change it accordingly
-    premium: 200,
-    honeymoon: 300,
+    standard: 2999,    //change it accordingly
+    premium: 5599,
+    honeymoon: 7599,
   };
 
   function calculatePrice() {
     const selectedRoomType = roomTypeDropdown.value;
     const selectedPrice = roomPrices[selectedRoomType];
     priceInput.value = selectedPrice;
+    const numRooms = parseInt(numRoomsInput.value);
+    const totalPrice = selectedPrice * numRooms;
+    priceInput.value = totalPrice;
   }
   roomTypeDropdown.addEventListener('change', calculatePrice);
+  numRoomsInput.addEventListener('input', calculatePrice);
+
   function calculateNumberOfNights() {
     const checkInDate = new Date(document.getElementById('checkIn').value);
     const checkOutDate = new Date(document.getElementById('checkOut').value);
+
+    if (checkInDate > checkOutDate) {
+      alert("Check-out date must be greater than the Check-in date.");
+      return;
+    }
     
     const timeDifference = checkOutDate - checkInDate;
     const numberOfNights = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
   
     document.getElementById('numberOfNights').value = numberOfNights;
-    
   }
-
   
-  calculateTotalAmount();
 
   function handleFormSubmit(event) {
     event.preventDefault();
@@ -35,7 +41,7 @@ const roomTypeDropdown = document.getElementById('room-type');
   }
   
   document.getElementById('billingForm').addEventListener('submit', handleFormSubmit);
-  document.getElementById('rooms').addEventListener('change', calculateTotalAmount);
+  document.getElementById('rooms').addEventListener('change', calculatePrice);
   document.getElementById('checkIn').addEventListener('change', calculateNumberOfNights);
   document.getElementById('checkOut').addEventListener('change', calculateNumberOfNights);
   
