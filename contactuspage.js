9e56a6b37e9261f5ccceef4867ee80628b7b43bf
ShravanJump <cst.21bctg94@silicon.ac.in>
2023-07-28 04:10:31 +0530

    Final Commit

--- contactuspage.js
+++ contactuspage.js
@@ -1,12 +1,6 @@
-function openwebsite(){
-    window.location.href="index.html";
-}
-
-function redirect(){
-    window.location.href="bookings.html";
-}
-function abtus(){
-    window.location.href="finalabtus.html";
+function success(event){
+    event.preventDefault();
+    alert("Your message has been delivered. Please wait, we will get back to you via email");
 }
 function logout(){
     const token=localStorage.getItem('token');
